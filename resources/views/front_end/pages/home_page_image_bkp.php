4a0a403e5bafcbe82c9db0bc40f1ced47cb45c12
Rajesh Mishra <Rajeshm@Lenskart.in>
2020-05-09 18:46:41 +0530

    Change Html

---
+++ resources/views/front_end/pages/home_page_image_bkp.php
@@ -0,0 +1,179 @@
+<div data-vc-full-width="true" data-vc-full-width-init="false" data-vc-stretch-content="true" class="vc_row wpb_row vc_row-fluid vc_custom_1571430219837 vc_row-no-padding vc_row_1140879858" >
+                <div class="wpb_column vc_column_container vc_col-sm-12 vc_column_1668033939">
+                  <div class="vc_column-inner ">
+                    <div class="wpb_wrapper">
+                      
+                      <div id="menuall-content">
+                        <div class="menu-nav">
+                        <div class="process-menu">
+                          <span class="sh-filter-item active">
+                            Current 
+                          </span> 
+                        </div>
+                     
+                      <div class="current-menu"> 
+                        <span class="sh-filter-item active">
+                          Current Contest
+                        </span> 
+                        <span class="sh-filter-item" >
+                          Winners
+                        </span>                    
+                    </div>
+
+                    <div class="select-all">
+                      <label>Previus Contest</label>
+                      <select>
+                        <option>Last Hours</option>>
+                        <option>1:30AM to 2:30PM</option>
+                        <option>2:30AM to 3:30PM</option>
+                        <option>3:30AM to 4:30PM</option>
+                        <option>4:30AM to 5:30PM</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  </div>
+                  
+                      <div id="portfolio-fancy-7F42qo5iwf" class="sh-portfolio-fancy sh-portfolio-fancy-columns3">
+                        <div class="sh-portfolio-fancy-item category-packaging" id="portfolio-661">
+                          <div class="sh-portfolio-fancy-itemc-container">
+                            <img class="sh-portfolio-img" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/31/2019/04/greg-t-386887-unsplash1-1.jpg" alt="" />
+                            <div class="sh-portfolio-fancy-item-overlay">
+                              <div class="sh-portfolio-fancy-item-overlay-container">
+                                <div class="sh-portfolio-fancy-item-overlay-categories">
+                                  <a href="#popupVideo" data-rel="popup" data-position-to="window" class="sh-portfolio-category sh-heading-font ui-btn ui-corner-all ui-shadow ui-btn-inline">View Photo</a>           
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="sh-portfolio-fancy-item category-offices" id="portfolio-654">
+                          <div class="sh-portfolio-fancy-itemc-container">
+                            <img class="sh-portfolio-img" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/31/2019/04/simone-hutsch-oqlh6RsrYB0-unsplash1-1-777x1024.jpg" alt="" />
+                            <div class="sh-portfolio-fancy-item-overlay">
+                              <div class="sh-portfolio-fancy-item-overlay-container">
+                                <div class="sh-portfolio-fancy-item-overlay-categories">
+                                  <a href="#" class="sh-portfolio-category sh-heading-font popup">View Photo</a>            
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="sh-portfolio-fancy-item category-graphic-design" id="portfolio-653">
+                          <div class="sh-portfolio-fancy-itemc-container">
+                            <img class="sh-portfolio-img" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/31/2019/04/simone-hutsch-_M4SLgyL3Ps-unsplash-1.jpg" alt="" />
+                            <div class="sh-portfolio-fancy-item-overlay">
+                              <div class="sh-portfolio-fancy-item-overlay-container">
+                                <div class="sh-portfolio-fancy-item-overlay-categories">
+                                  <a href="#" class="sh-portfolio-category sh-heading-font popup">View Photo</a>            
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="sh-portfolio-fancy-item category-graphic-design" id="portfolio-666">
+                          <div class="sh-portfolio-fancy-itemc-container">
+                            <img class="sh-portfolio-img" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/31/2019/04/Scene-43-1.jpg" alt="" />
+                            <div class="sh-portfolio-fancy-item-overlay">
+                              <div class="sh-portfolio-fancy-item-overlay-container">
+                                <div class="sh-portfolio-fancy-item-overlay-categories">
+                                  <a href="#" class="sh-portfolio-category sh-heading-font">View Photo</a>            
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="sh-portfolio-fancy-item category-packaging" id="portfolio-652">
+                          <div class="sh-portfolio-fancy-itemc-container">
+                            <img class="sh-portfolio-img" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/31/2019/04/inside-weather-RQOABS5RH0M-unsplash-1-774x1024.jpg" alt="" />
+                            <div class="sh-portfolio-fancy-item-overlay">
+                              <div class="sh-portfolio-fancy-item-overlay-container">
+                                <div class="sh-portfolio-fancy-item-overlay-categories">
+                                  <a href="#" class="sh-portfolio-category sh-heading-font">View Photo</a>            
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="sh-portfolio-fancy-item category-shopping" id="portfolio-647">
+                          <div class="sh-portfolio-fancy-itemc-container">
+                            <img class="sh-portfolio-img" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/31/2019/04/simone-hutsch-wkmciuuhL54-unsplash-1.jpg" alt="" />
+                            <div class="sh-portfolio-fancy-item-overlay">
+                              <div class="sh-portfolio-fancy-item-overlay-container">
+                                <div class="sh-portfolio-fancy-item-overlay-categories">
+                                  <a href="#" class="sh-portfolio-category sh-heading-font">View Photo</a>            
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="sh-portfolio-fancy-item category-art" id="portfolio-646">
+                          <div class="sh-portfolio-fancy-itemc-container">
+                            <img class="sh-portfolio-img" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/31/2019/04/simone-hutsch-vU_kdB2PSTY-unsplash-1-777x1024.jpg" alt="" />
+                            <div class="sh-portfolio-fancy-item-overlay">
+                              <div class="sh-portfolio-fancy-item-overlay-container">
+                                <div class="sh-portfolio-fancy-item-overlay-categories">
+                                  <a href="#" class="sh-portfolio-category sh-heading-font">View Photo</a>            
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="sh-portfolio-fancy-item category-offices" id="portfolio-645">
+                          <div class="sh-portfolio-fancy-itemc-container">
+                            <img class="sh-portfolio-img" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/31/2019/04/simone-hutsch-LZSpneVsnt0-unsplash-1.jpg" alt="" />
+                            <div class="sh-portfolio-fancy-item-overlay">
+                              <div class="sh-portfolio-fancy-item-overlay-container">
+                                <div class="sh-portfolio-fancy-item-overlay-categories">
+                                  <a href="#" class="sh-portfolio-category sh-heading-font">View Photo</a>            
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="sh-portfolio-fancy-item category-hard-press" id="portfolio-643">
+                          <div class="sh-portfolio-fancy-itemc-container">
+                            <img class="sh-portfolio-img" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/31/2019/04/essentialiving-h4ry_4h80VA-unsplash-1-777x1024.jpg" alt="" />
+                            <div class="sh-portfolio-fancy-item-overlay">
+                              <div class="sh-portfolio-fancy-item-overlay-container">
+                                <div class="sh-portfolio-fancy-item-overlay-categories">
+                                  <a href="#" class="sh-portfolio-category sh-heading-font">View Photo</a>            
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="sh-portfolio-fancy-item category-shopping" id="portfolio-642">
+                          <div class="sh-portfolio-fancy-itemc-container">
+                            <img class="sh-portfolio-img" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/31/2019/04/simone-hutsch-UpW6pfsN5I-unsplash-1.jpg" alt="" />
+                            <div class="sh-portfolio-fancy-item-overlay">
+                              <div class="sh-portfolio-fancy-item-overlay-container">
+                                <div class="sh-portfolio-fancy-item-overlay-categories">
+                                  <a href="#" class="sh-portfolio-category sh-heading-font">View Photo</a>            
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="sh-portfolio-fancy-item category-graphic-design" id="portfolio-174">
+                          <div class="sh-portfolio-fancy-itemc-container">
+                            <img class="sh-portfolio-img" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/31/2019/04/inside-weather-BayIzHKkjM4-unsplash-1.jpg" alt="" />
+                            <div class="sh-portfolio-fancy-item-overlay">
+                              <div class="sh-portfolio-fancy-item-overlay-container">
+                                <div class="sh-portfolio-fancy-item-overlay-categories">
+                                  <a href="#" class="sh-portfolio-category sh-heading-font">View Photo</a>            
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <style type="text/css">
+                        #portfolio-fancy-7F42qo5iwf .sh-portfolio-fancy-item-overlay-bg {background: rgba(0,0,0,0.55);background: -moz-linear-gradient(45deg, rgba(0,0,0,0.55) 0%, rgba(41,76,255,0.53) 100%);background: -webkit-linear-gradient(45deg, rgba(0,0,0,0.55) 0%, rgba(41,76,255,0.53) 100%);background: linear-gradient(45deg, rgba(0,0,0,0.55) 0%, rgba(41,76,255,0.53) 100%);}#portfolio-fancy-7F42qo5iwf .sh-portfolio-fancy-item {padding: 0 0px; margin: 0px 0;}            
+                      </style>
+                      <style type="text/css"> .vc_column_1668033939:not(.vc_parallax):not(.jarallax) { overflow: center!important; position: relative; }</style>
+                    </div>
+                  </div>
+                </div>
+                <style type="text/css">@media (max-width: 800px) { #content .vc_row_1140879858, .sh-footer-template .vc_row_1140879858, .sh-header-template .vc_row_1140879858 { padding: 0px 0px 0px 0px!important;}}</style>
+              </div>
