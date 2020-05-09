a1ccc9a6ed30fddcf1d769d43b293fed4fa9b485
geekraghav <raghvendra.pathak@lenskart.in>
2020-05-09 19:21:14 +0530

    merged #2

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/', 'SiteController@index')->name('home');
 Route::get('/image/api', 'SiteController@ApiCall');
 Route::get('/winner/images', 'SiteController@winnerApi');
 Route::get('/share', 'SiteController@share');
+
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
     Route::get('/approve-image', 'HomeController@approveImage')->name('approve');
     Route::get('/mark-as-winner', 'HomeController@markAsWinnner')->name('winner');
     Route::get('/image-list', 'HomeController@getAllImages')->name('imageslist');
