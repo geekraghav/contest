8f4766252c75506a57683cc6586565770c9dd541
geekraghav <raghvendra.pathak@lenskart.in>
2020-08-13 01:32:34 +0530

    add campaign 6 condtion

--- app/Home.php
+++ app/Home.php
@@ -11,7 +11,7 @@ class Home extends Model
     public static function getContestData()
     {
 
-        $result = Home::select('id', 'user_id', 'conversation_id', 'mobile_no', 'image_url', 'is_approved', 'approved_by', 'is_winner', 'created_at')->orderBy('created_at','DESC')->paginate(25);
+        $result = Home::select('id', 'user_id', 'conversation_id', 'mobile_no', 'image_url', 'is_approved', 'approved_by', 'is_winner', 'created_at')->where('campaign_name','6')->orderBy('created_at','DESC')->paginate(25);
 
 
         if ($result->isNotEmpty()) {
